407c61062604c82fb68d52218acc4cedbcb15d90
WEBXICO-MX <weiss.uttab@gmail.com>
2016-09-06 11:45:11 -0500

    Imagenes que conforman el Calendario Escolar 2016-2017 ITSLV

---
+++ db/calendario_escolar_INSERTS.sql
@@ -0,0 +1,14 @@
+SELECT * FROM calendario_escolar
+
+INSERT INTO calendario_escolar VALUES(1,'Agosto 2016','http://www.uttab.edu.mx/ITSLV_api/resources/img/calendario_escolar/2016-2017/agosto_2016.png',GETDATE(),NULL,1)
+INSERT INTO calendario_escolar VALUES(2,'Septiembre 2016','http://www.uttab.edu.mx/ITSLV_api/resources/img/calendario_escolar/2016-2017/septiembre_2016.png',GETDATE(),NULL,1)
+INSERT INTO calendario_escolar VALUES(3,'Octubre 2016','http://www.uttab.edu.mx/ITSLV_api/resources/img/calendario_escolar/2016-2017/octubre_2016.png',GETDATE(),NULL,1)
+INSERT INTO calendario_escolar VALUES(4,'Noviembre 2016','http://www.uttab.edu.mx/ITSLV_api/resources/img/calendario_escolar/2016-2017/noviembre_2016.png',GETDATE(),NULL,1)
+INSERT INTO calendario_escolar VALUES(5,'Diciembre 2016','http://www.uttab.edu.mx/ITSLV_api/resources/img/calendario_escolar/2016-2017/diciembre_2016.png',GETDATE(),NULL,1)
+INSERT INTO calendario_escolar VALUES(6,'Enero 2017','http://www.uttab.edu.mx/ITSLV_api/resources/img/calendario_escolar/2016-2017/enero_2017.png',GETDATE(),NULL,1)
+INSERT INTO calendario_escolar VALUES(7,'Febrero 2017','http://www.uttab.edu.mx/ITSLV_api/resources/img/calendario_escolar/2016-2017/febrero_2017.png',GETDATE(),NULL,1)
+INSERT INTO calendario_escolar VALUES(8,'Marzo 2017','http://www.uttab.edu.mx/ITSLV_api/resources/img/calendario_escolar/2016-2017/marzo_2017.png',GETDATE(),NULL,1)
+INSERT INTO calendario_escolar VALUES(9,'Abril 2017','http://www.uttab.edu.mx/ITSLV_api/resources/img/calendario_escolar/2016-2017/abril_2017.png',GETDATE(),NULL,1)
+INSERT INTO calendario_escolar VALUES(10,'Mayo 2017','http://www.uttab.edu.mx/ITSLV_api/resources/img/calendario_escolar/2016-2017/mayo_2017.png',GETDATE(),NULL,1)
+INSERT INTO calendario_escolar VALUES(11,'Junio 2017','http://www.uttab.edu.mx/ITSLV_api/resources/img/calendario_escolar/2016-2017/junio_2017.png',GETDATE(),NULL,1)
+INSERT INTO calendario_escolar VALUES(12,'Julio 2017','http://www.uttab.edu.mx/ITSLV_api/resources/img/calendario_escolar/2016-2017/julio_2017.png',GETDATE(),NULL,1)
